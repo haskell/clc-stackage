14b868d6fd87c5106ffcb7ecebc070a3a5afe764
Georgi Lyubenov <godzbanebane@gmail.com>
2022-05-31 10:36:39 +0300

    Add a nix shell for building

---
+++ flake.nix
@@ -0,0 +1,45 @@
+# Note that this is probably an overestimation.
+{
+  description = "Build dependencies for clc-stackage";
+  inputs = {
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    with nixpkgs.legacyPackages.x86_64-linux;
+    {
+      devShell.x86_64-linux = mkShell {
+        buildInputs = [
+          cabal-install
+          libffi.dev
+          libGL
+          xorg.libX11
+          zlib
+          pkg-config
+          xorg.libXcursor
+          xorg.libXrandr
+          xorg.libXxf86vm
+          xorg.libXi
+          bzip2
+          curl
+          libGLU
+          xorg.libXau
+          xorg.libXinerama
+          blas
+          pcre
+          icu
+          lzlib
+          xz
+          lapack
+          expat
+          file
+          systemdMinimal
+          elfutils
+          xorg.libXext.dev
+        ];
+      };
+    };
+}
