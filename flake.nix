ecfd4a7dc6ed17d546a6b16eaf2ec37ba85db942
Tommy Bidne <tbidne@protonmail.com>
2023-03-31 18:14:51 +1300

    Update nix for cabal 3.8

--- flake.nix
+++ flake.nix
@@ -6,14 +6,24 @@
       url = "github:edolstra/flake-compat";
       flake = false;
     };
+    nixpkgs-old.url = "github:nixos/nixpkgs/17b62c338f2a0862a58bb6951556beecd98ccda9";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, ... }:
-    with nixpkgs.legacyPackages.x86_64-linux;
+  outputs = { self, nixpkgs-old, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs-old = import nixpkgs-old {
+        inherit system;
+      };
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in with pkgs-old;
     {
-      devShell.x86_64-linux = mkShell {
+      devShell.x86_64-linux = pkgs-old.mkShell {
         buildInputs = [
-          cabal-install
+          pkgs.cabal-install
           libffi.dev
           libGL
           xorg.libX11
