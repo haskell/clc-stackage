2ac9d9a04f3a7abcce80e349c23364885dbc3705
Tommy Bidne <tbidne@protonmail.com>
2023-08-07 10:33:25 +1200

    Update to stackage nightly-2023-08-06
    New external dependencies (haskell -> dep):
    - alsa-pcm -> alsa
    - coinor-clp -> clp
    - cairo -> xdmcp
    - gio -> libselinux, libsepol, mount
    - glib -> libpcre2
    - glpk-headers -> glpk
    - gtk -> gtk2
    - hmpfr -> mpfr
    - jack -> jack2
    - nettle -> nettle
    - pango -> datrie, fribidi, libthai
    - saltine -> libsodium
    - zeromq4-haskell -> libzmq

--- flake.nix
+++ flake.nix
@@ -6,57 +6,84 @@
       url = "github:edolstra/flake-compat";
       flake = false;
     };
-    nixpkgs-old.url = "github:nixos/nixpkgs/17b62c338f2a0862a58bb6951556beecd98ccda9";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs-old, nixpkgs, ... }:
+  outputs = { self, nixpkgs, ... }:
     let
       system = "x86_64-linux";
-      pkgs-old = import nixpkgs-old {
-        inherit system;
-      };
       pkgs = import nixpkgs {
         inherit system;
       };
-    in with pkgs-old;
+      compiler = pkgs.haskell.packages.ghc962;
+
+      deps = with pkgs; [
+        alsa-lib
+        cabal-install
+        clp
+        fribidi
+        glpk
+        gtk2
+        jack2
+        libdatrie
+        libffi.dev
+        libGL
+        libselinux
+        libsepol
+        libsodium
+        libthai
+        util-linux
+        mpfr
+        nettle
+        pcre2
+        zeromq
+        xorg.libX11
+        zlib
+        pkg-config
+        xorg.libXcursor
+        xorg.libXdmcp
+        xorg.libXrandr
+        xorg.libXxf86vm
+        xorg.libXi
+        bzip2
+        curl
+        libGLU
+        xorg.libXau
+        xorg.libXinerama
+        blas
+        pcre
+        icu
+        lzlib
+        xz
+        lapack
+        expat
+        file
+        systemdMinimal
+        elfutils
+        xorg.libXext.dev
+        pango
+        glib
+        libxml2
+        numactl
+        protobuf
+        openal
+        gmp
+      ];
+    in
     {
-      devShell.x86_64-linux = pkgs-old.mkShell {
-        buildInputs = [
-          pkgs.cabal-install
-          libffi.dev
-          libGL
-          xorg.libX11
-          zlib
-          pkg-config
-          xorg.libXcursor
-          xorg.libXrandr
-          xorg.libXxf86vm
-          xorg.libXi
-          bzip2
-          curl
-          libGLU
-          xorg.libXau
-          xorg.libXinerama
-          blas
-          pcre
-          icu
-          lzlib
-          xz
-          lapack
-          expat
-          file
-          systemdMinimal
-          elfutils
-          xorg.libXext.dev
-          pango
-          glib
-          libxml2
-          numactl
-          protobuf
-          openal
-          gmp
-        ];
+      devShells.x86_64-linux = {
+        default = pkgs.mkShell {
+          buildInputs = deps;
+        };
+
+        # This shell is useless wrt the intended purpose of this repo,
+        # as the whole point is building stackage w/ a custom ghc. It exists
+        # primarily to test that there is nothing wrong with the package set
+        # itself for nix users, as building some of the libs with a custom
+        # ghc is a challenge on NixOS.
+        with-ghc = pkgs.mkShell {
+          buildInputs = deps ++ [ compiler.ghc ];
+        };
       };
     };
 }
